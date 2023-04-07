3829822c4b4984a764323dca6724a0b05ed67ce2
saprasona <128086441+saprasona@users.noreply.github.com>
2023-04-07 10:35:08 +0530

    Delete Module:3.1 directory

--- Module:3.1/Myself.c
+++
@@ -1,9 +0,0 @@
-//Displays this information using printf:your name,birtdate,age,address//
-#include<stdio.h>
-int main()
-{ printf("My Name Is Sapra Sona\n");
-  printf("My Birthdate Is 15/10/1988\n");
-  printf("I Am 35 Years old\n");
-  printf("Address: 82,Laxminagar soc.,Sarthana Jakatnaka, Surat\n");
-    return 0;
-}
