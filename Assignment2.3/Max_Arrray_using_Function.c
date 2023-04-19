cbdde0f3f7cbbc5b95b46d8755e86a8993eb28ad
saprasona <128086441+saprasona@users.noreply.github.com>
2023-04-19 10:00:54 +0530

    Add files via upload

--- Assignment2.3/Max_Arrray_using_Function.c
+++ Assignment2.3/Max_Arrray_using_Function.c
@@ -6,12 +6,14 @@ int main()
     int a[100],a_size,Max_Num,i;
     printf("Enter the size of array=");
     scanf("%d",&a_size);
-    printf("Enter the eMaxements into array\n");
+    printf("Enter the elements into array\n");
+    {
     for(i=0;i<a_size;i++)
     scanf("%d",&a[i]);
     Max_Num=findMax(a,a_size);
     printf("Max. number is=%d\n",Max_Num);
-   return 0;
+    }
+       return 0;
 }
 int findMax(int a[],int a_size)
 {
